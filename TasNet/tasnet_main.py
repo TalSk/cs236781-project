Fixed bug in lr halving, maybe fixed a bug in f0 calculation

TasNet/tasnet_main.py
@@ -37,9 +37,9 @@ if __name__ == '__main__':
             tf.keras.layers.Conv1D(args.B, 1, 1),
             "1d_deconv":
             tf.keras.layers.Dense(args.L, use_bias=False),
-            "f0_deconv": tf.keras.Sequential((tf.keras.layers.Dense(2 * args.N, use_bias=False),
-                                              tf.keras.layers.Dense(2 * args.N, use_bias=False),
-                                              tf.keras.layers.Dense(128, use_bias=False))),
+            "f0_deconv": tf.keras.Sequential((tf.keras.layers.Dense(2 * args.N, activation="relu", use_bias=False),
+                                              tf.keras.layers.Dense(2 * args.N, activation="relu", use_bias=False),
+                                              tf.keras.layers.Dense(128, activation="relu", use_bias=False))),
             "loudness_deconv": tf.keras.Sequential((tf.keras.layers.Dense(2 * args.N, use_bias=False),
                                               tf.keras.layers.Dense(2 * args.N, use_bias=False),
                                               tf.keras.layers.Dense(1, use_bias=False)))
@@ -100,7 +100,8 @@ if __name__ == '__main__':
 
         if args.mode == 'train':
             lr = args.learning_rate
-            valid_scores = [-1] * 2
+            no_improve_count = 0
+            prev_loss = 2000
 
             for epoch in range(1, args.max_epoch + 1):
 
@@ -132,15 +133,19 @@ if __name__ == '__main__':
                         valid_loss_sum += cur_loss * args.batch_size
                         valid_iter_cnt += args.batch_size
                     except tf.errors.OutOfRangeError:
-                        cur_sdr = (valid_loss_sum / valid_iter_cnt)
+                        epoch_loss = (valid_loss_sum / valid_iter_cnt)
+                        
+                        if epoch_loss >= prev_loss:
+                            no_improve_count += 1
+                            if no_improve_count >= 3:
+                                lr *= 0.8
+                        else:
+                            no_improve_count = 0
+                        prev_loss = epoch_loss
 
-                        valid_scores.append(cur_sdr)
-                        if max(valid_scores[-3:]) < valid_sdr:
-                            lr *= 0.7
-
-                        logging.info('validation loss = {:5f}'.format(cur_sdr))
-                        if cur_sdr > valid_sdr:
-                            valid_sdr = cur_sdr
+                        logging.info('validation loss = {:5f}'.format(epoch_loss))
+                        if epoch_loss < valid_sdr:
+                            valid_sdr = epoch_loss
                             saver.save(
                                 sess,
                                 args.checkpoint_path,

TasNet/tasnet_tf.py
@@ -122,7 +122,7 @@ class TasNet:
         # self.loss = tf.reduce_mean(-sdr) / self.C
         probs = [tf.nn.softplus(y) + 1e-3 for y in f0_deconved]
         probs = [prob / tf.reduce_sum(prob, axis=-1, keepdims=True) for prob in probs]
-        output_f0s = tf.squeeze(self._compute_midi(probs), axis=-1)
+        output_f0s = tf.squeeze(self._compute_f0_hz(probs), axis=-1)
 
         # C, B, F
         output_loudnesses = loudness_deconved
@@ -146,19 +146,19 @@ class TasNet:
         difference = sum(list_difference)
         return tf.reduce_mean(tf.abs(difference))
 
-    def _compute_midi(self, probs):
-        """Computes the midi from a distribution over D intervals."""
+    def _compute_f0_hz(self, probs):
+        """Computes the f0 in herz from a distribution over D MIDI intervals."""
         # probs: [B, T, D]
         depth = int(probs[0].shape[-1])
 
         midi_bins = tf.constant(
-            1.0 * np.arange(depth).reshape((1, 1, -1)),
+            1.0 * np.arange(depth).reshape((1, 1, -1)) / depth,
             dtype=tf.float32)  # [1, 1, D]
-        midi_bins = tf.map_fn(ddsp.core.midi_to_hz, midi_bins)
 
-        f0_midi = [tf.reduce_sum(
-            midi_bins * prob, axis=-1, keepdims=True) for prob in probs]  # [B, T, 1]
-        return f0_midi
+        f0_hz = [ddsp.core.midi_to_hz(
+            127.0 * tf.reduce_sum(
+            midi_bins * prob, axis=-1, keepdims=True)) for prob in probs]  # [B, T, 1]
+        return f0_hz
 
     def _channel_norm(self, inputs, name):
         # inputs: [batch_size, some len, channel_size]